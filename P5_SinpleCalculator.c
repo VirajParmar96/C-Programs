b52557bb0b53890a7bf6baa79cc91f78ff51fdac
VirajParmar96 <viraj.parmar960@gmail.com>
2025-02-25 20:32:41 +0530

    5. Simple Calculator 6. Factorial of a Number

---
+++ P5_SinpleCalculator.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch(operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0)
+                result = num1 / num2;
+            else {
+                printf("Error! Division by zero is not allowed.\n");
+                return 1;
+            }
+            break;
+        default:
+            printf("Invalid operator.\n");
+            return 1;
+    }
+
+    printf("Result: %.2lf\n", result);
+    return 0;
+}
+
+// Output:
+// Enter an operator (+, -, *, /): *
+// Enter two numbers: 377.846 8.5 
+// Result: 3211.69
