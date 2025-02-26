bee8a8c86ef3ab0e5d0cd0699c3bc667fae1aed2
VirajParmar96 <viraj.parmar960@gmail.com>
2025-02-26 12:18:25 +0530

    Reverse a Number

---
+++ P7_ReverseNumber.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num, reversed = 0, remainder;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num /= 10;
+    }
+
+    printf("Reversed number: %d\n", reversed);
+    return 0;
+}
+
+// Output:
+// Enter an integer: 876
+// Reversed number: 678
