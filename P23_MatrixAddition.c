57f68aed2133d980ac6c11247f86b0c565dcdf83
VirajParmar96 <viraj.parmar960@gmail.com>
2025-03-09 11:32:08 +0530

    23. Matrix Addition

---
+++ P23_MatrixAddition.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j;
+
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    int matrix1[rows][cols], matrix2[rows][cols], sum[rows][cols];
+
+    printf("Enter elements of first matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    printf("Enter elements of second matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            sum[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    printf("Sum of matrices:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+// Output:
+// Enter the number of rows and columns: 2 2
+// Enter elements of first matrix:
+// 1 2
+// 3 4
+// Enter elements of second matrix:
+// 5 6
+// 7 8
+// Sum of matrices:
+// 6 8
+// 10 12
